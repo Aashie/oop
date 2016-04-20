38b4502dc1dd03ad2e7905886cf6ab195cc43644
Anmol Chalise <anmol.chalise.c4@gmail.com>
2016-04-20 18:04:24 +0545

    added student classs

--- add.php
+++ add.php
@@ -1,11 +1,12 @@
 <?php
 
-require 'classes/User.php';
+require 'vendor/autoload.php';
 
 $name = $_POST['name'];
 $bio = $_POST['bio'];
 $password = $_POST['password'];
 
+new Student();
 
 $user = new User();
 
